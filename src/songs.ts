441fc97dbb04e800709aa571e6bcbc160f66bf4d
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-06-23 00:16:25 +0200

    Added logic to function areSameSong

--- src/songs.ts
+++ src/songs.ts
@@ -1,11 +1,7 @@
 import { Song } from "./songsData";
 
 export const areSameSong = (song: Song, songTitle: string): boolean => {
-  let areSame: boolean;
-  // Asígnale a la variable areSame un true o un false dependiendo de si
-  // el título de la canción recibida coincide con el título recibido
-
-  areSame = true; // Cambia esta línea por tu código
+  let areSame: boolean = song.title === songTitle ? true : false;
 
   return areSame;
 };
